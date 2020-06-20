889f8b9bc39b7c15c9b9e46a1cbd242d997f7db3
Selbin_C_R <selbinzr@gmail.com>
2020-06-20 22:52:01 +0530

    avoided duplicate entries

--- server/gateway/weatherGateway.js
+++ server/gateway/weatherGateway.js
@@ -2,12 +2,12 @@ const axios = require('axios')
 const cron = require('node-cron')
 const db = require('../database')
 const dotEnv = require('dotenv')
-
+let endDate = 0
 dotEnv.config()
 
 /**
  * @description fetch weather data using open-weather-map api
- * @param {string} city 
+ * @param {string} city
  */
 
 const getWeatherData = async city => {
@@ -26,18 +26,21 @@ module.exports = () => {
     cities.forEach(async city => {
       let result = {}
       const response = await getWeatherData(city)
-      response.data.list.forEach((data) => {
-        result = {
-          name: response.data.city.name,
-          lat: response.data.city.coord.lat,
-          long: response.data.city.coord.lon,
-          id: response.data.city.id,
-          time: data.dt,
-          dateString: data.dt_txt,
-          temp: data.main.temp
+      response.data.list.forEach((data, i) => {
+        if (data.dt > endDate) {
+          result = {
+            name: response.data.city.name,
+            lat: response.data.city.coord.lat,
+            long: response.data.city.coord.lon,
+            id: response.data.city.id,
+            time: data.dt,
+            dateString: data.dt_txt,
+            temp: data.main.temp
+          }
+          db.weatherData.insert(result)
         }
-        db.weatherData.insert(result)
       })
+      endDate = response.data.list[response.data.list.length].dt
     })
   })
 }
