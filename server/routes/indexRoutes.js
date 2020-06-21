4c2cfd25171dc57b03c8c98d7351bbc096f88d64
Selbin_C_R <selbinzr@gmail.com>
2020-06-21 12:32:52 +0530

    updated routes

--- server/routes/indexRoutes.js
+++ server/routes/indexRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const WeatherHandler = require('../controllers/weatherHandler')
 
-router.get('/info/:date/:time/:location/:low/:high', WeatherHandler.checkTempRange)
+router.get('/info/:time/:location/:low/:high', WeatherHandler.checkTempRange)
 router.get('/location/:startTime/:endTime/:low/:high', WeatherHandler.getLocation)
 router.get('/time/:location/:low/:high', WeatherHandler.getTime)
 
