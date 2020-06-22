872c02c8f0db3b24bb5a18a328736d05cb8bb570
Selbin_C_R <selbinzr@gmail.com>
2020-06-23 00:04:24 +0530

    structured code

--- server/app.js
+++ server/app.js
@@ -1,38 +1,14 @@
 const express = require('express')
-const cron = require('node-cron')
-const axios = require('axios')
 const dotEnv = require('dotenv')
-const db = require('./model/store')
+const indexRoutes = require('./routes/indexRoutes')
+const cronJob = require('./gateway/weatherGateway')
+
 dotEnv.config()
 
+cronJob()
+
 const app = express()
-const cities = JSON.parse(process.env.CITY)
 
-const downloadData = async city => {
-  const url = `http://api.openweathermap.org/data/2.5/forecast?id=${city}&appid=${process.env.API_KEY}`
-  try {
-    const response = await axios.get(url)
-    return response
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-cron.schedule('0 0 0 * * *', async () => {
-  await cities.forEach(async city => {
-    const result = {}
-    const response = await downloadData(city)
-    result.name = response.data.city.name
-    result.lat = response.data.city.coord.lat
-    result.lon = response.data.city.coord.lon
-    result.id = response.data.city.id
-    result.data = []
-    response.data.list.forEach((data) => {
-      result.data.push({ time: data.dt, dateString: data.dt_txt, temp: data.main.temp })
-    })
-    console.log(result)
-    db.data.insert(result)
-  })
-})
+app.use('/weather', indexRoutes)
 
 app.listen(process.env.APP_PORT, () => console.log('listening to: ', process.env.APP_PORT))
