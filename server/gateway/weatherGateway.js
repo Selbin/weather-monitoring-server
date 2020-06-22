d07af5439031e145de0186a1589f24ea4d9e8573
Selbin_C_R <selbinzr@gmail.com>
2020-06-23 00:09:07 +0530

    structed folder

---
+++ server/gateway/weatherGateway.js
@@ -0,0 +1,43 @@
+const axios = require('axios')
+const cron = require('node-cron')
+const db = require('../database')
+const dotEnv = require('dotenv')
+
+dotEnv.config()
+
+/**
+ * @description fetch weather data using open-weather-map api
+ * @param {string} city 
+ */
+
+const getWeatherData = async city => {
+  const url = `http://api.openweathermap.org/data/2.5/forecast?id=${city}&appid=${process.env.API_KEY}&units=${process.env.TEMP_UNIT}`
+  try {
+    const response = await axios.get(url)
+    return response
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+module.exports = () => {
+  cron.schedule('0 0 0 * * *', () => {
+    const cities = JSON.parse(process.env.CITY)
+    cities.forEach(async city => {
+      let result = {}
+      const response = await getWeatherData(city)
+      response.data.list.forEach((data) => {
+        result = {
+          name: response.data.city.name,
+          lat: response.data.city.coord.lat,
+          long: response.data.city.coord.lon,
+          id: response.data.city.id,
+          time: data.dt,
+          dateString: data.dt_txt,
+          temp: data.main.temp
+        }
+        db.weatherData.insert(result)
+      })
+    })
+  })
+}
